e8abd8a6fc1157f4c05902dea100d3420952ae74
Zeytun48 <dahiyezeytun56@gmail.com>
2024-11-20 08:39:13 -0800

    Set up Django project, created api app, and added Book model with migrations

---
+++ api_project/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+from django.db import models
+
+# Create your models here.
