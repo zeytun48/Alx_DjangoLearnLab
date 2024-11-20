aded538d83f508d3906b51d4a2615db8e6de471b
Zeytun48 <dahiyezeytun56@gmail.com>
2024-11-20 10:07:11 -0800

    Updated views, URLs, and added serializer for book API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
+# api/views.py
+from rest_framework import generics
+from bookshelf.models import Book  # Ensure this import is correct based on your Book model
+from .serializers import BookSerializer  # Import the serializer you just created
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()  # This retrieves all Book objects from the database
+    serializer_class = BookSerializer  # This tells the view which serializer to use
 from django.shortcuts import render
 
 # Create your views here.
