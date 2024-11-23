763d6fdb851006c16deb2ff5768ac440aa85599e
Zeytun48 <dahiyezeytun56@gmail.com>
2024-11-22 21:43:45 -0800

    Set up custom user model in advanced_features_and_security project

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+
